eac98e88271dd23e7d0429297f492eb9966884e3
rajeswarimandadhi <rajeswarimandadhi@gmail.com>
2022-11-15 16:45:38 +0530

    backend

---
+++ WebAPI/WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public DBModel db = new DBModel();
+
+        //api/getname
+        public IQueryable<Login> getLogin()
+        {
+            return db.Login;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class RegisterController : ApiController
+    {
+        public DBModel db = new DBModel();
+        
+        //api/getename
+        public IQueryable<Register> getRegister()
+        {
+            return db.Register;
+        }
+        
+
+    }
+}
